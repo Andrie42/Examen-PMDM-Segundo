b724257d19fe2841fa98bc9388ef81b7ee736b86
Andrea Amado Lain <andrea.amado486@comunidadunir.net>
2026-02-17 18:56:02 +0100

    primer commit

---
+++ app/src/main/java/com/app/andreaamadolain_pmdm_segunda/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.app.andreaamadolain_pmdm_segunda
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.app.andreaamadolain_pmdm_segunda.ui.theme.AndreaAmadoLain_PMDM_SegundaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndreaAmadoLain_PMDM_SegundaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndreaAmadoLain_PMDM_SegundaTheme {
+        Greeting("Android")
+    }
+}
